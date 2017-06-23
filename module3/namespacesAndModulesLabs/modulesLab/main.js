74c92a365af7d50c153e614863e659e24ba0adcd
Jancsi <gulyasjancsi@gmail.com>
2017-06-23 15:03:08 +0200

    brackets are fixed now

--- module3/namespacesAndModulesLabs/modulesLab/main.js
+++ module3/namespacesAndModulesLabs/modulesLab/main.js
@@ -1,6 +1,6 @@
 "use strict";
 exports.__esModule = true;
-var arrayUtils_js_1 = require("./arrayUtils.js"); //hiba lesz amíg nem létezik, de .js-ként kell
+var arrayUtils_js_1 = require("./arrayUtils.js");
 var array1 = [
     1,
     2,
@@ -18,13 +18,13 @@ var array2 = [
 function callArrayUtil(util, array, array2) {
     if (array2 === void 0) { array2 = null; }
     document.getElementById("output").innerHTML = arrayUtils_js_1["default"][util](array, array2).toString();
-    var _loop_1 = function (property) {
-        document.getElementById(property).onclick = function () {
-            callArrayUtil(property, array1, array2);
-        };
+}
+var _loop_1 = function (property) {
+    document.getElementById(property).onclick = function () {
+        callArrayUtil(property, array1, array2);
     };
-    for (var property in arrayUtils_js_1["default"]) {
-        _loop_1(property);
-    }
+};
+for (var property in arrayUtils_js_1["default"]) {
+    _loop_1(property);
 }
 document.getElementById("output").innerHTML = array1.toString();
